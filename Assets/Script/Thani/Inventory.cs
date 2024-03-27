6dfc314b91ecc2f0e232552689df9ad7a3004e0b
MTA_Temp <mohammad.thani@yahoo.com>
2024-03-27 21:31:58 +0400

    Inventory WIP

---
+++ Assets/Script/Thani/Inventory.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    bool isOn;
+    public GameObject inventory;
+
+    public void Use()
+    {
+        if (isOn == true)
+        {
+            isOn = false;
+            inventory.SetActive(false);
+        }
+
+        if (isOn == false)
+        {
+            isOn = true;
+            inventory.SetActive(true);
+        }
+    }
+}

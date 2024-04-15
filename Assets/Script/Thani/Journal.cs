7821b23f44d13037ceca6e4f1900b7555e6770e6
MTA_Temp <mohammad.thani@yahoo.com>
2024-04-15 14:08:48 +0400

    Journal WIP

---
+++ Assets/Script/Thani/Journal.cs
@@ -0,0 +1,23 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Journal : MonoBehaviour
+{
+    [SerializeField]
+    public Image journal;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            journal.enabled = true;
+        }
+
+        if (other.CompareTag("Player"))
+        {
+            journal.enabled = false;
+        }
+    }
+}

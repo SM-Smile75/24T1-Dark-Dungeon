856011c16663d43379f8ac9033c4c4ccb3192536
SaifAl75 <s.almheiri@student.saedubai.com>
2024-04-25 11:39:42 +0400

    Tutorial Changes
    Added the ability for players to Open the box and picking up the Journal.
    
    The Player now cannot leave the Tutorial Room without picking up the Journal

--- Assets/Script/Saif/NextLevel.cs
+++ Assets/Script/Saif/NextLevel.cs
@@ -9,22 +9,39 @@ public class NextLevel : MonoBehaviour, IInteractable
         ReadyToExplore();
     }
     public GameObject readyBox;
+    public GameObject notReadyBox;
+    public GameObject theJournal;
     PlayerUI thePlayer;
 
     void Start()
     {
         thePlayer = GameObject.Find("PlayerUI").GetComponent<PlayerUI>();
+        notReadyBox.SetActive(false);
     }
     public void ReadyToExplore()
     {
+        if (theJournal.activeSelf)
+        {
+            notReadyBox.SetActive(true);
+            thePlayer.TextBoxStart();
+        }
+        else { 
         readyBox.SetActive(true);
         thePlayer.ReadyToMove();
+    
+        }
     }
-
     public void NotReady()
     {
+        notReadyBox.SetActive(false);
         readyBox.SetActive(false);
         thePlayer.ContinueGame();
     }
-   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && notReadyBox.activeSelf)
+        {
+            NotReady();
+        }
+    }
 }
